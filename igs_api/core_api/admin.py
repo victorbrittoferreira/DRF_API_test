1c0543f8c6c182e50b88bd0dbe1685a4e5f82ecf
Victor de Britto Ferreira <victorbrittoferreira@gmail.com>
2022-01-14 00:48:15 -0300

    .

--- igs_api/core_api/admin.py
+++ igs_api/core_api/admin.py
@@ -3,6 +3,8 @@ from core_api import models
 
 @admin.register(models.Employee)
 class EmployeeAdmin(admin.ModelAdmin):
+    fields = ['employee_name', 'email', 'department']
     list_display = ['employee_name', 'email', 'department']
+    search_fields = ['employee_name', 'email', 'department']
 
 # Register your models here.
